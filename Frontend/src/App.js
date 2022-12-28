4e1e2ac378717b3b58f9619a7cf9d73ef5a794ed
rahul.rauniyar <rahul.rauniyar@otssolutions.com>
2022-12-28 19:26:45 +0530

    change@1.0

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
-import Login from './components/login';
-
+import Cart from './components/Cart/Cart';
+import LoginSignUpModal from './components/Modal/LoginSignUpModal';
+// import { Router, Route } from 'react-router-dom';
+// import Navbar from './components/Navbar/Navbar';
+// import { Container } from 'semantic-ui-react';
+// import Login from './components/login';
 function App() {
-  return (
-    <div className="App">
-     <Login />
-    </div>
-  );
+	return (
+		<div className="App">
+			<br />
+			<br />
+			<br />
+			<Cart />
+			<br />
+			<br />
+			<br />
+			<LoginSignUpModal title="login" btnType="Login" />
+			<LoginSignUpModal title="Sign Up" btnType="Sign Up" />
+		</div>
+	);
 }
 
 export default App;
---
+++ Frontend/src/components/Cart/Cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Icon, Label } from 'semantic-ui-react';
+
+export default function Cart() {
+	return (
+		// <Menu.Item as="a">
+		<>
+			<Label color="red">22</Label>
+			<Icon name="mail" /> Messages
+		</>
+	);
+}
