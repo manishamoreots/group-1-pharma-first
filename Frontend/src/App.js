57954dd16cba9cdeb42c16cffc6cbed718d34f4e
Manisha More <manisha.more@otssolutions.com>
2022-12-29 11:25:39 +0530

    login-page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Login from './components/login';
-
+import Login from './components/login-page/login';
 function App() {
   return (
     <div className="App">
-     <Login />
+      <Login title="Login"/>
+      <Login title="Signup"/>
     </div>
   );
 }
