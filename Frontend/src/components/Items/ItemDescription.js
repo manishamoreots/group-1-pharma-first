fd43fede050d571bb71d1d156596f032e6a6ef63
rahul.rauniyar <rahul.rauniyar@otssolutions.com>
2023-01-02 12:18:25 +0530

    s

--- Frontend/src/components/Items/ItemDescription.js
+++ Frontend/src/components/Items/ItemDescription.js
@@ -1,33 +1,33 @@
-import React from 'react';
-import { Button, Container, Dropdown, Grid, Header, Image, Menu, Segment } from 'semantic-ui-react';
+import React from "react";
+import { Button, Container, Dropdown, Grid, Header, Image, Menu, Segment } from "semantic-ui-react";
 const data = {
 	id: 1,
 	categoryId: 1,
-	itemName: 'Virgo Healthcare Protivir Diet- Nutritional Supplement for Diabetic',
-	itemPrice: '₹2333',
+	itemName: "Virgo Healthcare Protivir Diet- Nutritional Supplement for Diabetic",
+	itemPrice: "₹2333",
 	highlights: [
-		'Protein supplement for specific nutritional needs of diabetic patients, people having high cholesterol level and problems with the weight management',
-		'It has low fat content and no added sugar',
-		'Provides the right nutrition to stay healthy and active',
-		'Enriched with antioxidants and essential vitamins and minerals to meet the increased requirements of diabetics',
+		"Protein supplement for specific nutritional needs of diabetic patients, people having high cholesterol level and problems with the weight management",
+		"It has low fat content and no added sugar",
+		"Provides the right nutrition to stay healthy and active",
+		"Enriched with antioxidants and essential vitamins and minerals to meet the increased requirements of diabetics",
 	],
 	desc: {
 		keybenefit: [
-			'It ensures beneficial effects in general debility, malnutrition, neuropathies, stress, and strains',
-			'Protein supplement for specific nutritional needs of diabetic patients, people having high cholesterol level and problems with the weight management',
-			'It is also effective in weight management and pregnancy',
-			'It helps release anabolic hormones that stimulate muscle growth',
+			"It ensures beneficial effects in general debility, malnutrition, neuropathies, stress, and strains",
+			"Protein supplement for specific nutritional needs of diabetic patients, people having high cholesterol level and problems with the weight management",
+			"It is also effective in weight management and pregnancy",
+			"It helps release anabolic hormones that stimulate muscle growth",
 		],
-		directions: ['Take Virgo Healthcare Protivir Diet Protein Powder (Sugar-Free)as directed by the physician.'],
-		Safetyinfo: ['Read the label carefully before use', 'Do not exceed the recommended dose', 'Keep out of the reach of children'],
+		directions: ["Take Virgo Healthcare Protivir Diet Protein Powder (Sugar-Free)as directed by the physician."],
+		Safetyinfo: ["Read the label carefully before use", "Do not exceed the recommended dose", "Keep out of the reach of children"],
 	},
 	itemImage:
-		'https://onemg.gumlet.io/l_watermark_346,w_120,h_120/a_ignore,w_120,h_120,c_fit,q_auto,f_auto/cropped/zl35i9inmpsoj2tmga2a.jpg',
+		"https://onemg.gumlet.io/l_watermark_346,w_120,h_120/a_ignore,w_120,h_120,c_fit,q_auto,f_auto/cropped/zl35i9inmpsoj2tmga2a.jpg",
 };
 const options = [
-	{ key: 1, text: '1 box', value: 1 },
-	{ key: 2, text: '2 Box', value: 2 },
-	{ key: 3, text: '3 Box', value: 3 },
+	{ key: 1, text: "1 box", value: 1 },
+	{ key: 2, text: "2 Box", value: 2 },
+	{ key: 3, text: "3 Box", value: 3 },
 ];
 const ItemDescription = () => {
 	return (
