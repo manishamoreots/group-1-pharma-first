0f84a60036b63a938ebaf21797bd75018aad49fa
Manisha More <manisha.more@otssolutions.com>
2023-01-02 18:32:41 +0530

    2 Jan 23

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,17 +1,18 @@
 import "./App.css";
 import CardComponent from "./components/category-carousel-pages/Card";
-import FooterComponent from "./components/footer-page/footercomponent";
-import Login from "./components/login-page/login";
-import MenuBar from "./components/menu-bar/menu-list";
+
+import SearchLocation from "./components/location/searchlocation";
+
+import MainSlider from "./components/main-slider/main-slider";
+// import MenuBar from "./components/menu-bar/menu-list";
 
 function App() {
   return (
     <div className="App">
-      <Login title="Login" />
-      <Login title="Signup" />
-      <MenuBar />
+      <SearchLocation />
+      {/* <MenuBar /> */}
       <CardComponent />
-      <FooterComponent />
+      <MainSlider />
     </div>
   );
 }
