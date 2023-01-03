6872ccf63aa9f72ff7bb578240ac749393e7ee4f
Manisha More <manisha.more@otssolutions.com>
2023-01-04 00:04:10 +0530

    3rd jan

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -6,19 +6,21 @@ import LoginSignUpModal from "./components/Modal/LoginSignUpModal";
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Common/Footer";
 import ItemDescription from "./components/Items/ItemDescription";
+import CategoryGrid from "./components/Category-Card/category-grid";
 // import Hello from './components/Hello';
 function App() {
 	return (
 		<div className="App">
-			<Routes>
+			{/* <Routes>
 				<Route path="/" element={<Home />} />
-				<Route path="/cart" element={<Cart />} />
-				<Route path="/itemDesc/:id" element={<ItemDescription />} />
+				<Route path="/cart" element={<Cart />} /> */}
+				<CategoryGrid />
+				{/* <Route path="/itemDesc/:id" element={<ItemDescription />} />
 				<Route path="/dashboard" element={<Dashboard />} />
 				<Route path="/login" element={<LoginSignUpModal title="login" btnType="Login" />} />
 				<Route path="/signUp" element={<LoginSignUpModal title="Sign Up" btnType="Sign Up" />} />
 			</Routes>
-			<Footer />
+			<Footer /> */}
 		</div>
 	);
 }
