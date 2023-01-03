4f53dea552954eba18ad659145621fc52b0da8e8
arun.kumar <arun.kumar@otssolutions.com>
2023-01-03 16:56:36 +0530

    contact us page added

--- Frontend/src/components/Cart/Cart.js
+++ Frontend/src/components/Cart/Cart.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import { Icon, Label } from 'semantic-ui-react';
+import React from "react";
+import { Icon, Label, Grid, Image, Container } from "semantic-ui-react";
 
 export default function Cart() {
-	return (
-		// <Menu.Item as="a">
-		<>
-			<Label color="red">22</Label>
-			<Icon name="mail" /> Messages
-		</>
-	);
+  return (
+    <Container>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={8}>
+            <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+          </Grid.Column>
+          <Grid.Column width={8}>
+            <Image src="https://react.semantic-ui.com/images/wireframe/paragraph.png" />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Container>
+  );
 }
--- Frontend/src/components/Reducer/categoryReducer.js
+++ Frontend/src/components/Reducer/categoryReducer.js
@@ -2,12 +2,12 @@ import { createSlice, current } from "@reduxjs/toolkit";
 import { category } from "../../Data/Category";
 const initialState = { data: { categories: [...category] } };
 export const categorySlice = createSlice({
-	name: "category",
-	initialState: initialState,
-	reducers: {
-		getCategory: (state, action) => {
-			state.data = { categories: current(state.data.categories) };
-		},
-	},
+  name: "category",
+  initialState: initialState,
+  reducers: {
+    getCategory: (state, action) => {
+      state.data = { categories: current(state.data.categories) };
+    },
+  },
 });
 export const { getCategory } = categorySlice.actions;
