ad1107ee43c991fda1cb872d8f8cb414adfa5ecd
arun.kumar <arun.kumar@otssolutions.com>
2023-01-03 16:56:36 +0530

    contact us page added

--- Frontend/src/components/Contact/Contact.js
+++ Frontend/src/components/Contact/Contact.js
@@ -7,6 +7,7 @@ import {
   Select,
   Container,
   Image,
+  Grid,
 } from "semantic-ui-react";
 import img from "../image/contact.webp";
 const genderOptions = [
@@ -18,60 +19,62 @@ const genderOptions = [
 const Contact = () => (
   <Container>
     <h2 style={{ marginBottom: 40 }}>Contact Us</h2>
-    <div style={{ display: "flex", justifyContent: "space-evenly" }}>
-      <div>
-        <Image src={img} size="large" />
-      </div>
-      <div>
-        <Form>
-          <Form.Group widths="equal">
+    <Grid>
+      <Grid.Row>
+        <Grid.Column width={8}>
+          <Image src={img} size="large" />
+        </Grid.Column>
+        <Grid.Column width={8}>
+          <Form>
+            <Form.Group widths="equal">
+              <Form.Field
+                id="form-input-control-first-name"
+                control={Input}
+                label="First name"
+                placeholder="First name"
+              />
+              <Form.Field
+                id="form-input-control-last-name"
+                control={Input}
+                label="Last name"
+                placeholder="Last name"
+              />
+              <Form.Field
+                control={Select}
+                options={genderOptions}
+                label={{
+                  children: "Gender",
+                  htmlFor: "form-select-control-gender",
+                }}
+                placeholder="Gender"
+                search
+                searchInput={{ id: "form-select-control-gender" }}
+              />
+            </Form.Group>
             <Form.Field
-              id="form-input-control-first-name"
+              id="form-input-control-error-email"
               control={Input}
-              label="First name"
-              placeholder="First name"
+              label="Email"
+              placeholder="arun@otssolutions.com"
             />
             <Form.Field
-              id="form-input-control-last-name"
-              control={Input}
-              label="Last name"
-              placeholder="Last name"
+              id="form-textarea-control-opinion"
+              control={TextArea}
+              label="Message"
+              placeholder="Type Your Message here"
             />
-            <Form.Field
-              control={Select}
-              options={genderOptions}
-              label={{
-                children: "Gender",
-                htmlFor: "form-select-control-gender",
-              }}
-              placeholder="Gender"
-              search
-              searchInput={{ id: "form-select-control-gender" }}
-            />
-          </Form.Group>
-          <Form.Field
-            id="form-input-control-error-email"
-            control={Input}
-            label="Email"
-            placeholder="arun@otssolutions.com"
-          />
-          <Form.Field
-            id="form-textarea-control-opinion"
-            control={TextArea}
-            label="Message"
-            placeholder="Type Your Message here"
-          />
 
-          <Form.Field
-            id="form-button-control-public"
-            control={Button}
-            content="Send Message"
-            secondary
-            // label="Label with htmlFor"
-          />
-        </Form>
-      </div>
-    </div>
+            <Form.Field
+              id="form-button-control-public"
+              control={Button}
+              content="Send Message"
+              secondary
+              // label="Label with htmlFor"
+            />
+          </Form>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
   </Container>
 );
 
--- Frontend/src/components/category-card/category-grid.js
+++ Frontend/src/components/category-card/category-grid.js
@@ -3,25 +3,27 @@ import { Link } from "react-router-dom";
 import { Card, Icon, Image } from "semantic-ui-react";
 
 const CategoryGrid = () => {
-	return (
-		<>
-			<Card>
-				<Image src="/images/avatar/large/matthew.png" wrapped ui={false} />
-				<Card.Content>
-					<Card.Header>Matthew</Card.Header>
-					<Card.Meta>
-						<span className="date">Joined in 2015</span>
-					</Card.Meta>
-					<Card.Description>Matthew is a musician living in Nashville.</Card.Description>
-				</Card.Content>
-				<Card.Content extra>
-					<Link>
-						<Icon name="user" />
-						22 Friends
-					</Link>
-				</Card.Content>
-			</Card>
-		</>
-	);
+  return (
+    <>
+      <Card>
+        <Image src="/images/avatar/large/matthew.png" wrapped ui={false} />
+        <Card.Content>
+          <Card.Header>Matthew</Card.Header>
+          <Card.Meta>
+            <span className="date">Joined in 2015</span>
+          </Card.Meta>
+          <Card.Description>
+            Matthew is a musician living in Nashville.
+          </Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <Link>
+            <Icon name="user" />
+            22 Friends
+          </Link>
+        </Card.Content>
+      </Card>
+    </>
+  );
 };
 export default CategoryGrid;
