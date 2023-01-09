3ce3eb3b97a9acef4357ac565bf3ad7a361d66e9
Manisha More <manisha.more@otssolutions.com>
2023-01-09 13:02:37 +0530

    mm

--- Frontend/src/components/Dashboard/Dashboard.js
+++ Frontend/src/components/Dashboard/Dashboard.js
@@ -11,204 +11,240 @@ import swal from "sweetalert";
 import { getLocation } from "../Reducer/commonReducer";
 
 const Dashboard = () => {
-	const user = JSON.parse(localStorage.getItem("user"));
-	const dispatch = useDispatch();
-	const [loading, setLoading] = useState(true);
-	const { locationInfo } = useSelector((state) => state.common.data);
-	const [editView, setEditView] = useState(false);
-	let navigate = useNavigate();
-	useEffect(() => {
-		if (user) {
-			console.log(user);
-		} else {
-			navigate("/");
-		}
-	}, [user]);
-	useEffect(() => {
-		setTimeout(() => {
-			setLoading(false);
-		}, 1000);
-		navigator.geolocation.getCurrentPosition((position) => {
-			const { latitude, longitude } = position.coords;
-			dispatch(getLocation({ latitude, longitude }));
-		});
-	}, [loading]);
+  const user = JSON.parse(localStorage.getItem("user"));
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
+  const { locationInfo } = useSelector((state) => state.common.data);
+  const [editView, setEditView] = useState(false);
+  let navigate = useNavigate();
+  useEffect(() => {
+    if (user) {
+      console.log(user);
+    } else {
+      navigate("/");
+    }
+  }, [user]);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+    navigator.geolocation.getCurrentPosition((position) => {
+      const { latitude, longitude } = position.coords;
+      dispatch(getLocation({ latitude, longitude }));
+    });
+  }, [loading]);
 
-	const accountDelete = () => {
-		swal({
-			title: "Are you sure?",
-			text: "Once deleted, you will not be able to recover this Account",
-			icon: "warning",
-			buttons: true,
-			dangerMode: true,
-		}).then((willDelete) => {
-			if (willDelete) {
-				swal("Your Account Deleted Successfully", {
-					icon: "success",
-				});
-				dispatch(logout());
-				navigate("/");
-			} else {
-				swal("Your Account is safe!");
-			}
-		});
-	};
+  const accountDelete = () => {
+    swal({
+      title: "Are you sure?",
+      text: "Once deleted, you will not be able to recover this Account",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        swal("Your Account Deleted Successfully", {
+          icon: "success",
+        });
+        dispatch(logout());
+        navigate("/");
+      } else {
+        swal("Your Account is safe!");
+      }
+    });
+  };
 
-	return (
-		<div>
-			{loading ? (
-				<div>
-					<Loader />
-				</div>
-			) : (
-				<Container>
-					<Breadcrumb style={{ borderBottom: "1px solid #777" }}>
-						<Breadcrumb.Section active={!editView} onClick={() => setEditView(false)}>
-							Personal Information
-						</Breadcrumb.Section>
-						<Breadcrumb.Divider>/</Breadcrumb.Divider>
-						<Breadcrumb.Section active={editView} onClick={() => setEditView(true)}>
-							EditProfile
-						</Breadcrumb.Section>
-					</Breadcrumb>
+  return (
+    <div>
+      {loading ? (
+        <div>
+          <Loader />
+        </div>
+      ) : (
+        <Container>
+          <Breadcrumb style={{ borderBottom: "1px solid #777" }}>
+            <Breadcrumb.Section
+              active={!editView}
+              onClick={() => setEditView(false)}
+            >
+              Personal Information
+            </Breadcrumb.Section>
+            <Breadcrumb.Divider>/</Breadcrumb.Divider>
+            <Breadcrumb.Section
+              active={editView}
+              onClick={() => setEditView(true)}
+            >
+              EditProfile
+            </Breadcrumb.Section>
+          </Breadcrumb>
 
-					<div style={{ display: "flex", justifyContent: "space-evenly" }}>
-						<div
-							style={{
-								width: "25%",
-								height: "50vh",
-								borderRadius: "20px",
-								marginTop: 30,
-								justifyContent: "center",
-							}}
-						>
-							<div>
-								<Image
-									src={user ? (user.picture ? user.picture : userLogo) : userLogo}
-									avatar
-									centered
-									size="small"
-									style={{ marginLeft: 69 }}
-								/>
-							</div>
-							<div>
-								<h3
-									style={{
-										textAlignLast: "center",
-										padding: 20,
-										marginLeft: 12,
-									}}
-								>
-									{user?.name}
-								</h3>
-							</div>
-							<div>
-								<Button onClick={accountDelete}>Delete My Account</Button>
-							</div>
-						</div>
-						{editView ? (
-							<EditProfile EditView={setEditView} />
-						) : (
-							<div
-								style={{
-									width: "75%",
-									justifyContent: "space-evenly",
-									marginTop: 40,
-								}}
-							>
-								<div
-									style={{
-										display: "flex",
-										justifyContent: "space-evenly",
-										flexDirection: "row",
-									}}
-								>
-									<div style={{ width: "80%" }}>
-										<div
-											style={{
-												boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
-												width: "80%",
-												height: "300px",
-												padding: "5%",
-												borderRadius: "5px",
-											}}
-										>
-											<div
-												style={{
-													display: "flex",
-													borderBottom: "1.5px solid teal",
-													marginBottom: "2%",
-												}}
-											>
-												<h4 style={{ fontFamily: "cursive" }}>Full Name : </h4>
-												<p style={{ marginLeft: "20px", fontSize: "15px", fontFamily: "cursive" }}>
-													{user?.name}
-												</p>
-											</div>
+          <div style={{ display: "flex" }}>
+            <div
+              style={{
+                width: "25%",
+                height: "50vh",
+                borderRadius: "20px",
+                marginTop: 30,
+              }}
+            >
+              <div>
+                <Image
+                  src={
+                    user ? (user.picture ? user.picture : userLogo) : userLogo
+                  }
+                  avatar
+                  centered
+                  size="small"
+                />
+              </div>
+              <div>
+                <h3
+                  style={{
+                    textAlignLast: "center",
+                    padding: 20,
+                    marginLeft: "5px",
+                  }}
+                >
+                  {user?.name}
+                </h3>
+              </div>
+              <div>
+                <Button onClick={accountDelete}>Delete My Account</Button>
+              </div>
+            </div>
+            {editView ? (
+              <EditProfile EditView={setEditView} />
+            ) : (
+              <div
+                style={{
+                  width: "75%",
+                  justifyContent: "space-evenly",
+                  marginTop: 40,
+                }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "space-evenly",
+                    flexDirection: "row",
+                  }}
+                >
+                  <div style={{ width: "80%" }}>
+                    <div
+                      style={{
+                        boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px",
+                        width: "80%",
+                        height: "300px",
+                        padding: "5%",
+                        borderRadius: "5px",
+                      }}
+                    >
+                      <div
+                        style={{
+                          display: "flex",
+                          borderBottom: "1.5px solid teal",
+                          marginBottom: "2%",
+                        }}
+                      >
+                        <h4 style={{ fontFamily: "cursive" }}>Full Name : </h4>
+                        <p
+                          style={{
+                            marginLeft: "20px",
+                            fontSize: "15px",
+                            fontFamily: "cursive",
+                          }}
+                        >
+                          {user?.name}
+                        </p>
+                      </div>
 
-											<div
-												style={{
-													display: "flex",
-													borderBottom: "1.5px solid teal",
-													marginBottom: "2%",
-												}}
-											>
-												<h4 style={{ fontFamily: "cursive" }}>Email Id : </h4>
-												<p style={{ marginLeft: "32px", fontSize: "15px", fontFamily: "cursive" }}>
-													{user?.email}
-												</p>
-											</div>
+                      <div
+                        style={{
+                          display: "flex",
+                          borderBottom: "1.5px solid teal",
+                          marginBottom: "2%",
+                        }}
+                      >
+                        <h4 style={{ fontFamily: "cursive" }}>Email Id : </h4>
+                        <p
+                          style={{
+                            marginLeft: "32px",
+                            fontSize: "15px",
+                            fontFamily: "cursive",
+                          }}
+                        >
+                          {user?.email}
+                        </p>
+                      </div>
 
-											<div
-												style={{
-													display: "flex",
-													borderBottom: "1.5px solid teal",
-													marginBottom: "2%",
-												}}
-											>
-												<h4 style={{ fontFamily: "cursive" }}>Mobile No : </h4>
-												<p style={{ marginLeft: "18px", fontSize: "15px", fontFamily: "cursive" }}>
-													{user.mobile ? user.mobile : "Please Update"}
-												</p>
-											</div>
+                      <div
+                        style={{
+                          display: "flex",
+                          borderBottom: "1.5px solid teal",
+                          marginBottom: "2%",
+                        }}
+                      >
+                        <h4 style={{ fontFamily: "cursive" }}>Mobile No : </h4>
+                        <p
+                          style={{
+                            marginLeft: "18px",
+                            fontSize: "15px",
+                            fontFamily: "cursive",
+                          }}
+                        >
+                          {user.mobile ? user.mobile : "Please Update"}
+                        </p>
+                      </div>
 
-											<div
-												style={{
-													display: "flex",
-													borderBottom: "1.5px solid teal",
-													marginBottom: "2%",
-												}}
-											>
-												<h4 style={{ fontFamily: "cursive" }}>Country : </h4>
-												<p style={{ marginLeft: "30px", fontSize: "15px", fontFamily: "cursive" }}>
-													{locationInfo.countryName
-														? locationInfo.countryName
-														: "Please Enable Location"}
-												</p>
-											</div>
+                      <div
+                        style={{
+                          display: "flex",
+                          borderBottom: "1.5px solid teal",
+                          marginBottom: "2%",
+                        }}
+                      >
+                        <h4 style={{ fontFamily: "cursive" }}>Country : </h4>
+                        <p
+                          style={{
+                            marginLeft: "30px",
+                            fontSize: "15px",
+                            fontFamily: "cursive",
+                          }}
+                        >
+                          {locationInfo.countryName
+                            ? locationInfo.countryName
+                            : "Please Enable Location"}
+                        </p>
+                      </div>
 
-											<div
-												style={{
-													display: "flex",
-													borderBottom: "1.5px solid teal",
-												}}
-											>
-												<h4 style={{ fontFamily: "cursive" }}>Address : </h4>
-												<p style={{ marginLeft: "30px", fontSize: "15px", fontFamily: "cursive" }}>
-													{locationInfo.city
-														? `${locationInfo.city}  ${locationInfo.principalSubdivision}`
-														: "Please Enable Location"}
-												</p>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						)}
-					</div>
-				</Container>
-			)}
-		</div>
-	);
+                      <div
+                        style={{
+                          display: "flex",
+                          borderBottom: "1.5px solid teal",
+                        }}
+                      >
+                        <h4 style={{ fontFamily: "cursive" }}>Address : </h4>
+                        <p
+                          style={{
+                            marginLeft: "30px",
+                            fontSize: "15px",
+                            fontFamily: "cursive",
+                          }}
+                        >
+                          {locationInfo.city
+                            ? `${locationInfo.city}  ${locationInfo.principalSubdivision}`
+                            : "Please Enable Location"}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+        </Container>
+      )}
+    </div>
+  );
 };
 export default Dashboard;
--- Frontend/src/components/Dashboard/EditProfile.js
+++ Frontend/src/components/Dashboard/EditProfile.js
@@ -6,89 +6,114 @@ import { EditUser } from "../Reducer/authReducer";
 import swal from "sweetalert";
 
 const EditProfile = ({ EditView }) => {
-	const user = JSON.parse(localStorage.getItem("user"));
-	const [userDetails, setUserDetails] = useState({
-		name: "",
-		email: "",
-		mobile: "",
-	});
-	const dispatch = useDispatch();
-	useEffect(() => {
-		setUserDetails(user);
-	}, []);
-	const change = (e) => {
-		const { name, value } = e.target;
-		setUserDetails((prevState) => ({
-			...prevState,
-			[name]: value,
-		}));
-	};
-	const update = () => {
-		dispatch(EditUser(userDetails));
-		swal({ text: "User Updated" });
-		EditView(false);
-		setUserDetails({
-			name: "",
-			email: "",
-			mobile: "",
-		});
-	};
+  const user = JSON.parse(localStorage.getItem("user"));
+  const [userDetails, setUserDetails] = useState({
+    name: "",
+    email: "",
+    mobile: "",
+  });
+  const dispatch = useDispatch();
+  useEffect(() => {
+    setUserDetails(user);
+  }, []);
+  const change = (e) => {
+    const { name, value } = e.target;
+    setUserDetails((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+  const update = () => {
+    dispatch(EditUser(userDetails));
+    swal({ text: "User Updated" });
+    EditView(false);
+    setUserDetails({
+      name: "",
+      email: "",
+      mobile: "",
+    });
+  };
 
-	return (
-		<Container>
-			<div
-				style={{
-					display: "flex",
-					justifyContent: "space-evenly",
-					marginTop: 50,
-				}}
-			>
-				<div
-					style={{
-						width: "75%",
-						justifyContent: "space-evenly",
-					}}
-				>
-					<div>
-						<div style={{ width: "80%" }}>
-							<div
-								style={{
-									width: "80%",
-									alignSelf: "center",
-									justifyContent: "flex-start",
-									height: "50vh",
-									borderRadius: "10px",
-								}}
-							>
-								<Input iconPosition="left" placeholder={userDetails?.name} style={{ width: "65vh" }} size="large">
-									<Icon name="user" />
-									<input name="name" value={userDetails?.name} onChange={change} />
-								</Input>
+  return (
+    <Container>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-evenly",
+          marginTop: 50,
+        }}
+      >
+        <div
+          style={{
+            width: "75%",
+            justifyContent: "space-evenly",
+          }}
+        >
+          <div>
+            <div style={{ width: "80%" }}>
+              <div
+                style={{
+                  width: "80%",
+                  alignSelf: "center",
+                  justifyContent: "flex-start",
+                  height: "50vh",
+                  borderRadius: "10px",
+                }}
+              >
+                <Input
+                  iconPosition="left"
+                  placeholder={userDetails?.name}
+                  style={{ width: "65vh" }}
+                  size="large"
+                >
+                  <Icon name="user" />
+                  <input
+                    name="name"
+                    value={userDetails?.name}
+                    onChange={change}
+                  />
+                </Input>
 
-								<Input iconPosition="left" placeholder={userDetails?.email} style={{ width: "65vh" }} size="large">
-									<Icon name="at" />
-									<input name="email" disabled value={userDetails?.email} onChange={change} />
-								</Input>
+                <Input
+                  iconPosition="left"
+                  placeholder={userDetails?.email}
+                  style={{ width: "65vh" }}
+                  size="large"
+                >
+                  <Icon name="at" />
+                  <input
+                    name="email"
+                    disabled
+                    value={userDetails?.email}
+                    onChange={change}
+                  />
+                </Input>
 
-								<Input
-									iconPosition="left"
-									placeholder={userDetails.mobile ? userDetails.mobile : "Please Update"}
-									style={{ width: "65vh" }}
-									size="large"
-								>
-									<Icon name="phone" />
-									<input name="mobile" value={userDetails.mobile} onChange={change} />
-								</Input>
+                <Input
+                  iconPosition="left"
+                  placeholder={
+                    userDetails.mobile ? userDetails.mobile : "Please Update"
+                  }
+                  style={{ width: "65vh" }}
+                  size="large"
+                >
+                  <Icon name="phone" />
+                  <input
+                    name="mobile"
+                    value={userDetails.mobile}
+                    onChange={change}
+                  />
+                </Input>
 
-								<div>
-									<Button onClick={update}>Edit Details</Button>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</Container>
-	);
+                <div>
+                  <Button onClick={update}>Edit Details</Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 };
 export default EditProfile;
